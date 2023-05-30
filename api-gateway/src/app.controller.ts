a74dbe97e70083876d69284ff91a3047d9817026
septun6 <abelenko18@gmail.com>
2023-05-30 17:51:29 +0300

    docker init and api-gateway basic added dockerfile setting post and get routes without connection with core-service app was checked with docker, everything is ok

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  // while files are not passed to core-service,
+  // they are stored in the dynamic dictionary
+  files= new Map<string, Express.Multer.File>;
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 5000000})
+      ]
+    })
+  ) file: Express.Multer.File) {
+    this.files[file.originalname] = file;
+    console.log(file);
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Map<string, Express.Multer.File>{
+    return this.files;
   }
 }
